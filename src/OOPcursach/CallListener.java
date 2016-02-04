1c00c4b0bc5d27a56d24621196e010a2c7b18daf
AnnaZemtsova <zemtsova18anna@gmail.com>
2016-02-04 22:53:19 +0200

    работа с вызовом

---
+++ src/OOPcursach/CallListener.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.net.*;
+
+public class CallListener {
+    private ServerSocket server;
+
+    public CallListener(ServerSocket server) {
+        this.server = server;
+    }
+
+    public void setServerSocket(ServerSocket server) {
+        this.server = server;
+    }
+
+    public Socket getSocket() {
+        Socket socket = null;
+        try {
+            socket = server.accept();
+        } catch (IOException e) {
+            System.out.println("Не удается подключиться");
+        }
+        return socket;
+    }
+}
---
+++ src/OOPcursach/CallListenerThread.java
@@ -0,0 +1,51 @@
+import java.util.Observable;
+import java.net.*;
+
+public class CallListenerThread extends Observable implements Runnable {
+    private Thread t = null;
+    private boolean isWorkFlag = true;
+    private CallListener callListener;
+    private Socket socket;
+    public final static  String itIsCallLisnenerThread="socket";
+
+
+    public CallListenerThread( int port){
+        try {
+            ServerSocket server = new ServerSocket(port);
+            callListener = new CallListener(server);
+        } catch (Exception e) {
+            System.out.println("Не могу создать такой порт: " + port);
+        }
+    }
+
+    public void CallListenerStart(String name){
+        t = new Thread(this, name);
+        t.start();
+    }
+
+    public void run() {
+        while(isWorkFlag){
+            synchronized (this) {
+                socket = null;
+                socket = callListener.getSocket();
+                setChanged();
+                notifyObservers(itIsCallLisnenerThread);
+            }
+        }
+    }
+
+
+    public void stop(){
+        synchronized (this) {
+            isWorkFlag = false;
+        }
+    }
+
+    public Socket getSocket(){
+        Socket socket1 = socket;
+        return socket1;
+    }
+
+
+
+}
